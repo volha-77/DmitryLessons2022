f2a97f7126122ca57989bc99f8177b1a6fed8533
volha-77 <volha-77@mail.ru>
2022-05-19 23:27:57 +0300

    feat: version 2.2, unite versions 1&2

--- recruitment/recruitment/RecruitmentSolution.cs
+++ recruitment/recruitment/RecruitmentSolution.cs
@@ -31,27 +31,51 @@ namespace recruitment
                 }
             };
 
-            ActionExecuter submitActionExecuter = new ActionExecuter("SubmitForApproval", actionConfig);
-            ActionExecuter approveActionExecuter = new ActionExecuter("Approve", actionConfig);
-            ActionExecuter rejectActionExecuter = new ActionExecuter("Reject", actionConfig);
+            Console.WriteLine("==============version 1=====================");
 
-            List<IActionExecuter> actionExecuters = new List<IActionExecuter>()
+            SubmitActionExecuter submitActionExecuter1 = new SubmitActionExecuter(actionConfig);
+            ApproveActionExecuter approveActionExecuter1 = new ApproveActionExecuter(actionConfig);
+            RejectActionExecuter rejectActionExecuter1 = new RejectActionExecuter(actionConfig);
+
+            List<IActionExecuter> actionExecuters1 = new List<IActionExecuter>()
             {
-                submitActionExecuter,
-                approveActionExecuter,
-                rejectActionExecuter
+                submitActionExecuter1,
+                approveActionExecuter1,
+                rejectActionExecuter1
+            };
+
+            var service1 = new Service(actionExecuters1);
+            // Invoke "Submit for Approval" for softwareEngineerVacancy
+            service1.ExecuteAction(softwareEngineerVacancy, "SubmitForApproval");
+            // Invoke "Approve" for softwareEngineerVacancy
+            service1.ExecuteAction(softwareEngineerVacancy, "Approve");
+            // Invoke "Submit for Approval" for frontEndEngineerVacancy
+            service1.ExecuteAction(frontEndEngineerVacancy, "SubmitForApproval");
+            // Invoke "Reject" for frontEndEngineerVacancy
+            service1.ExecuteAction(frontEndEngineerVacancy, "Reject");
+
+            Console.WriteLine("==============version 2=====================");
+            RecruitmentActionExecuter submitActionExecuter2 = new RecruitmentActionExecuter("SubmitForApproval", actionConfig);
+            RecruitmentActionExecuter approveActionExecuter2 = new RecruitmentActionExecuter("Approve", actionConfig);
+            RecruitmentActionExecuter rejectActionExecuter2 = new RecruitmentActionExecuter("Reject", actionConfig);
+
+            List<IActionExecuter> actionExecuters2 = new List<IActionExecuter>()
+            {
+                submitActionExecuter2,
+                approveActionExecuter2,
+                rejectActionExecuter2
             };
 
 
-            var service = new Service(actionExecuters);
+            var service2 = new Service(actionExecuters2);
             // Invoke "Submit for Approval" for softwareEngineerVacancy
-            service.ExecuteAction(softwareEngineerVacancy, "SubmitForApproval");
+            service2.ExecuteAction(softwareEngineerVacancy, "SubmitForApproval");
             // Invoke "Approve" for softwareEngineerVacancy
-            service.ExecuteAction(softwareEngineerVacancy, "Approve");
+            service2.ExecuteAction(softwareEngineerVacancy, "Approve");
             // Invoke "Submit for Approval" for frontEndEngineerVacancy
-            service.ExecuteAction(frontEndEngineerVacancy, "SubmitForApproval");
+            service2.ExecuteAction(frontEndEngineerVacancy, "SubmitForApproval");
             // Invoke "Reject" for frontEndEngineerVacancy
-            service.ExecuteAction(frontEndEngineerVacancy, "Reject");
+            service2.ExecuteAction(frontEndEngineerVacancy, "Reject");
         }
 
         public class Record
@@ -70,6 +94,15 @@ namespace recruitment
             public IDictionary<string, IDictionary<string, string>> ActionPerType { get; set; }
         }
 
+        public class ActionResult
+        {
+            public string TextResult { get; set; }
+
+            public ActionResult(string textResult)
+            {
+                TextResult = textResult;
+            }
+        }
 
         public interface IActionExecuter
         {
@@ -77,7 +110,84 @@ namespace recruitment
             ActionResult GetActionResult(string recordType);
         }
 
-        public class ActionExecuter : IActionExecuter
+        //classes for actions
+        //version 1
+        public class SubmitActionExecuter : IActionExecuter
+        {
+            public string ActionType { get; }
+
+            private ActionConfiguration _configuration { get; set; }
+
+            public SubmitActionExecuter(ActionConfiguration configuration)
+            {
+                ActionType = "SubmitForApproval";
+
+                _configuration = configuration;
+            }
+
+            public ActionResult GetActionResult(string recordType)
+            {
+                if (_configuration.ActionPerType.TryGetValue(recordType, out var actions) &&
+                    (actions.TryGetValue(ActionType, out var result)))
+                {
+                    return new ActionResult(result);
+                }
+
+                return new ActionResult("Submitted for approval ");
+            }
+        }
+
+        public class ApproveActionExecuter : IActionExecuter
+        {
+            public string ActionType { get; }
+            private ActionConfiguration _configuration { get; set; }
+
+            public ApproveActionExecuter(ActionConfiguration configuration)
+            {
+                ActionType = "Approve";
+
+                _configuration = configuration;
+            }
+
+            public ActionResult GetActionResult(string recordType)
+            {
+                if (_configuration.ActionPerType.TryGetValue(recordType, out var actions) &&
+                     (actions.TryGetValue(ActionType, out var result)))
+                {
+                    return new ActionResult(result);
+                }
+
+                return new ActionResult("Approved ");
+            }
+        }
+
+        public class RejectActionExecuter : IActionExecuter
+        {
+            public string ActionType { get; }
+
+            private ActionConfiguration _configuration { get; set; }
+
+            public RejectActionExecuter(ActionConfiguration configuration)
+            {
+                ActionType = "Reject";
+
+                _configuration = configuration;
+            }
+
+            public ActionResult GetActionResult(string recordType)
+            {
+                if (_configuration.ActionPerType.TryGetValue(recordType, out var actions) &&
+                    (actions.TryGetValue(ActionType, out var result)))
+                {
+                    return new ActionResult(result);
+                }
+
+                return new ActionResult("Rejected ");
+            }
+        }
+
+        //version 2
+        public class RecruitmentActionExecuter : IActionExecuter
         {
             private string _actionType;
 
@@ -85,7 +195,7 @@ namespace recruitment
 
             private ActionConfiguration _configuration { get; set; }
 
-            public ActionExecuter(string actionType, ActionConfiguration configuration)
+            public RecruitmentActionExecuter(string actionType, ActionConfiguration configuration)
             {
                 _actionType = actionType;
                 _configuration = configuration;
@@ -103,29 +213,20 @@ namespace recruitment
             }
         }
 
-        public class ActionResult
-        {
-            public string TextResult { get; set; }
-
-            public ActionResult(string textResult)
-            {
-                TextResult = textResult;
-            }
-        }
-
+        //class servise
         public class Service
         {
-            private List<IActionExecuter> _ationExecutersList = new List<IActionExecuter>();
+            private List<IActionExecuter> _actionExecutersList = new List<IActionExecuter>();
 
             public Service(IEnumerable<IActionExecuter> ationExecutersList)
             {
-                ationExecutersList.ToList().ForEach(x => _ationExecutersList.Add(x));
+                ationExecutersList.ToList().ForEach(x => _actionExecutersList.Add(x));
 
             }
 
             public void ExecuteAction(Record record, string action)
             {
-                foreach (var item in _ationExecutersList)
+                foreach (var item in _actionExecutersList)
                 {
                     if (item.ActionType.ToLower() == action.ToLower())
                     {
