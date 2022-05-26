355784256ddd9091e2af382ce6dc5f5af8d86fbb
volha-77 <volha-77@mail.ru>
2022-05-26 23:29:03 +0300

    init: CofeeMachine ver 1.1

---
+++ CofeeMachine/CofeeMachine/CofeeMachine/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CofeeMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<DrinkType> drinkTypes = new List<DrinkType>
+            {
+                DrinkType.cappucino,
+                DrinkType.espesso,
+                DrinkType.americano
+            };
+            CofeeMachine cofeeMachine = new CofeeMachine(drinkTypes);
+            var capuc = cofeeMachine.MakeDrink(DrinkType.cappucino);
+
+            if (capuc == null) throw new InvalidOperationException();
+        }
+    }
+
+    public class CofeeMachine: ICofeeMachine
+    {
+        public List<DrinkType> AvailableDrinkTypes { get; set; }
+
+        public IDictionary<Ingredient, double> AvailableIngredients { get; set; }
+
+        public IDictionary<Ingredient, double> SpentIngredients { get; }
+
+        public CofeeMachine(List<DrinkType> drinkTypes)
+        {
+            AvailableDrinkTypes = drinkTypes;
+        }
+
+        public Drink? MakeDrink(DrinkType drinkType)
+        {
+            if (AvailableDrinkTypes.Contains(drinkType))
+            {
+                Console.WriteLine($"Your {drinkType.ToString()} is ready");
+
+                return new Drink(drinkType);
+            }
+            else return null;
+        }
+    }
+
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Units Unit { get; set; }
+
+    }
+
+    public enum Units
+    {
+        liter,
+        kg,
+    }
+
+    public class Drink
+    {
+        public DrinkType Name { get; set; }
+
+        public double Volume { get; set; }
+
+        public IDictionary<Ingredient, double> Ingredients;
+        public double Price { get; set; }
+
+        public Drink(DrinkType name)
+        {
+            Name = name;
+        }
+    }
+
+    public interface ICofeeMachine
+    {
+        List<DrinkType> AvailableDrinkTypes { get; set; }
+
+        IDictionary<Ingredient, double> AvailableIngredients { get; set; }
+
+        IDictionary<Ingredient, double> SpentIngredients { get; }
+
+        public Drink MakeDrink(DrinkType drinkType);
+    }
+
+    public enum DrinkType
+    {
+        espesso,
+        cappucino,
+        americano,
+        glace
+    }
+}
